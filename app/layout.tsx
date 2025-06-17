c3196efdbd20ce600d93243416df06aa3fbc78e7
Karl <52241072+kungfury100@users.noreply.github.com>
2025-06-17 12:04:58 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,63 @@
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/contexts/theme-provider";
+import { Navbar } from "@/components/navbar";
+import { DM_Sans, DM_Mono } from "next/font/google";
+import { Footer } from "@/components/footer";
+import "@/styles/globals.css";
+import "@/styles/base.css";
+
+const sansFont = DM_Sans({
+  subsets: ["latin"],
+  variable: "--font-regular",
+  display: "swap",
+  weight: "400",
+});
+
+const monoFont = DM_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+  display: "swap",
+  weight: "400",
+});
+
+export const metadata: Metadata = {
+  title: "Moon Design System - Template",
+  metadataBase: new URL("https://ariadocs.vercel.app/"),
+  description:
+    "This comprehensive documentation template, crafted with Next.js and available as open-source, delivers a sleek and responsive design, tailored to meet all your project documentation requirements.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+        />
+      </head>
+      <body
+        className={`${sansFont.variable} ${monoFont.variable} font-regular antialiased tracking-wide`}
+        suppressHydrationWarning
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          <main className="sm:container mx-auto w-[90vw] h-auto scroll-smooth">
+            {children}
+          </main>
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,51 @@
+import { buttonVariants } from "@/components/ui/button";
+import { page_routes } from "@/lib/routes-config";
+import {
+  MoveUpRightIcon,
+  TerminalSquareIcon,
+  ChevronRightIcon,
+} from "lucide-react";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex sm:min-h-[87.5vh] min-h-[82vh] flex-col sm:items-center justify-center text-center sm:py-8 py-14">
+      <h1 className="text-[1.80rem] leading-8 sm:px-8 md:leading-[4.5rem] font-bold mb-4 sm:text-6xl text-left sm:text-center">
+        Build the best digital experiences
+      </h1>
+      <p className="mb-8 md:text-lg text-base  max-w-[1200px] text-secondary text-center sm:text-center">
+        Moon Design System is an open-source, complete design system. It's built
+        to help product teams across the world build better digital experiences.
+      </p>
+      <p className="mb-8 md:text-lg text-base  max-w-[1200px] text-secondary text-center sm:text-center">
+        Quickstart your project as a:
+      </p>
+      <div className="grid md:grid-cols-2 gap-5 pt-4">
+        <Link
+          className={buttonVariants({
+            variant: "secondary",
+            className: "sm:py-16 py-16 px-24 w-full flex flex-col",
+          })}
+          href={`/docs/quickstart/designer`}
+        >
+          <span className="md:text-lg font-bold">Designer</span>
+          <span className="flex items-center text-secondary md:text-lg pt-2 font-normal">
+            Get started with our Figma libraries.
+          </span>
+        </Link>
+        <Link
+          className={buttonVariants({
+            variant: "secondary",
+            className: "sm:py-16 py-16 px-24 w-full flex flex-col",
+          })}
+          href={`/docs/quickstart/developer`}
+        >
+          <span className="md:text-lg font-bold">Developer</span>
+          <span className="flex items-center text-secondary md:text-lg pt-2 font-normal">
+            Find installation guides to start building.
+          </span>
+        </Link>
+      </div>
+    </div>
+  );
+}
