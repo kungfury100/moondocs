4a79bd01c40dea1e9c885c5c78d8e4cf93dd0a70
Karl <52241072+kungfury100@users.noreply.github.com>
2025-06-18 10:44:01 +0300

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -54,7 +54,7 @@ export function Navbar() {
           <div className="flex items-center justify-between sm:gap-2">
             <div className="flex ml-4 sm:ml-0">
               <Link
-                href="https://github.com/nisabmohd/NexDocs"
+                href="https://github.com/kungfury100/moondocs"
                 className={buttonVariants({
                   variant: "ghost",
                   size: "icon",
